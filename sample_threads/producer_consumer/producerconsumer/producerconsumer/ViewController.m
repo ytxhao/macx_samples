0cc418af04443817cc3a466efe52e10160749d1c
hao.yu <hao.yu@ushow.media>
2022-01-19 18:39:53 +0800

    NSURLSession利用信号量实现同步请求

--- sample_threads/producer_consumer/producerconsumer/producerconsumer/ViewController.m
+++ sample_threads/producer_consumer/producerconsumer/producerconsumer/ViewController.m
@@ -34,12 +34,28 @@
 
 -(void)buttonClick{
     NSLog(@"你点击了切换模式");
-    for (int i = 0; i < 5;i++) {
+    for (int i = 0; i < 6;i++) {
         self.cnt++;
 //        __weak typeof(self)weakSelf = self;
         [self.messagingController put:^(){
 //            __strong typeof(weakSelf)strongSelf = weakSelf;
-            [NSThread sleepForTimeInterval:3.0];
+//            [NSThread sleepForTimeInterval:3.0];
+            NSString* url = [NSString stringWithFormat:@"https://%@/%d", @"www.baidu.com", i];//开播码率质量
+            NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
+            [request setCachePolicy:NSURLRequestReloadIgnoringCacheData];
+            [request setTimeoutInterval:10];
+            [request setHTTPMethod:@"GET"];
+            dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+            NSURLSession *session = [NSURLSession sharedSession];
+            NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+                NSLog(@"NSURLSessionDataTask %@", [NSThread currentThread]);
+//                NSLog(@"NSURLSessionDataTask %@", data);
+                [NSThread sleepForTimeInterval:4.0];
+                NSLog(@"NSURLSessionDataTask %@", response.URL);
+                dispatch_semaphore_signal(semaphore);
+            }];
+            [task resume];
+            dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
             NSLog(@"test runable block %d", i);
         }];
     }
